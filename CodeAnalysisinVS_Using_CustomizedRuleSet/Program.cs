62b34949a2066e6d8defd92db698e2eeca766123
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 09:18:41 +0530

    first commit

---
+++ CodeAnalysisinVS_Using_CustomizedRuleSet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysisinVS_Using_CustomizedRuleSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = a / b;
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
